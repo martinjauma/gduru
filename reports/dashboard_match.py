6f97c873ad468e4bc32eb8b5061bfd553e43b0e5
martinjauma <martin.jauma@gmail.com>
2024-08-03 18:56:32 -0300

    bd match filtro FECHA

--- reports/dashboard_match.py
+++ reports/dashboard_match.py
@@ -7,18 +7,33 @@ client = pymongo.MongoClient("mongodb+srv://martinjauma:Piston@clustergd.qny9kpp
 db = client["gestDep_db_json"]
 collection = db["match_URU"]
 
-
 # Función para cargar todos los datos de la colección
 def cargar_todos_los_datos():
     return list(collection.find())
 
+# Función para obtener valores únicos de la clave "FECHA"
+def obtener_valores_unicos_fecha():
+    return collection.distinct("FECHA")
 
-
+# Función para cargar los datos filtrados por fecha
+def cargar_datos_por_fecha(fecha):
+    return list(collection.find({"FECHA": fecha}))
 
 # Mostrar tabla con todos los datos de la colección
 st.title("Todos los Registros en la Base de Datos")
-st.subheader(" Match URU ")
+st.subheader("Match URU")
 
-datos_todos_registros = cargar_todos_los_datos()
-df_todos_registros = pd.DataFrame(datos_todos_registros)
-st.dataframe(df_todos_registros)
+# Obtener y mostrar valores únicos de la clave "FECHA"
+valores_unicos_fecha = obtener_valores_unicos_fecha()
+fecha_seleccionada = st.selectbox("Selecciona una fecha", valores_unicos_fecha)
+
+# Cargar y mostrar los datos filtrados por fecha
+if fecha_seleccionada:
+    datos_filtrados = cargar_datos_por_fecha(fecha_seleccionada)
+    if datos_filtrados:
+        df_filtrados = pd.DataFrame(datos_filtrados)
+        st.dataframe(df_filtrados)
+    else:
+        st.write("No se encontraron registros para la fecha seleccionada.")
+else:
+    st.write("Por favor, selecciona una fecha para mostrar los registros.")
